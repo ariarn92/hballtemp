c995eea4a9ce6fca65735c592d0af62483071cc0
ariarn92 <ariarn92@gmail.com>
2017-01-30 01:12:27 +0000

    Dates
    Added dates

--- HballStatsWeb(OLD)/client/views/application/nav.js
+++ HballStatsWeb(OLD)/client/views/application/nav.js
@@ -1,4 +1,18 @@
 Template.nav.events({
+  'submit form': function(event) {
+      event.preventDefault();
+
+      var date = event.target.date.value;
+
+      Meteor.listGamesByDate(date, function(error) {
+          if (error) {
+              Session.set('errorMessage', error.reason);
+          } else {
+              console.log("Successfully logged date!");
+          }
+      });
+  }
+
     'click #logout': function(event) {
         Meteor.logout(function(error) {
             if (error) {
--- HballStatsWeb(OLD)/client/views/auth/login.js
+++ HballStatsWeb(OLD)/client/views/auth/login.js
@@ -2,10 +2,10 @@ Template.login.events({
     'submit form': function(event) {
         event.preventDefault();
 
-        var email = event.target.email.value;
+        var team = event.target.team.value;
         var password = event.target.password.value;
 
-        Meteor.loginWithPassword(email, password, function(error) {
+        Meteor.loginWithPassword(team, password, function(error) {
             if (error) {
                 Session.set('errorMessage', error.reason);
             } else {
